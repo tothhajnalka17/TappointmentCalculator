e6d2c1df55eca856d0d6739dfe33f0ce1affd83e
HAJNALKA TÓOTH <tothhajnalka17@gmail.com>
2023-03-10 14:58:23 +0100

    Feat: add read/write to file functionality
    with error handling

--- server/index.js
+++ server/index.js
@@ -3,11 +3,36 @@ const cors = require('cors');
 const app = express();
 const PORT = 3001;
 let savedNum = 35674;
+let filePath = "SavedNum.txt";
 
 app.use( express.json() );
 app.use( cors() );
 
+const fs = require('fs');
+
+function readNumber(){
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        savedNum = data;
+      });
+}
+
+function writeNumber(number){
+    fs.writeFile(filePath, number, err => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("Number saved.");
+    }
+    });
+}
+
+
 app.get('/saved', (req, res) => {
+    readNumber();
     res.status(200).send({
         operation: `${savedNum}`,
     });
@@ -15,6 +40,7 @@ app.get('/saved', (req, res) => {
 
 app.post('/save', (req, res) => {
     savedNum = req.body.operation;
+    writeNumber(savedNum);
     if (!savedNum){
         res.status(400).send({ message: "I need a number."})
     } else {
